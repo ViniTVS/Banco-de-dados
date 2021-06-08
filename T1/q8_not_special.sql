456743c8352cc97a2627a3c40d3f403d8ca82e3f
Vinícius Teixeira Vieira dos Santos <vinisantos185@gmail.com>
2021-06-08 19:21:46 -0300

    acho que está tudo feito
    Signed-off-by: Vinícius Teixeira Vieira dos Santos <vinisantos185@gmail.com>

--- T1/q8_not_special.sql
+++ T1/q8_not_special.sql
@@ -1,13 +1,20 @@
 SELECT
-    C_NAME,
-    O_ORDERKEY,
-    O_COMMENT
-FROM
-    CUSTOMER
-LEFT JOIN ORDERS ON
-    CUSTOMER.C_CUSTKEY = ORDERS.O_CUSTKEY
---WHERE 
---	INSTR(O_COMMENT, 'special request')>0  
---	OR
---	O_ORDERKEY == NULL
-	;
+	COUNT(pedidos.soma)
+FROM (SELECT
+		sum(
+		case 
+			when INSTR(O_COMMENT, 'special request')>0 
+			then 1
+			else 0
+		end)
+		as soma
+	FROM
+		CUSTOMER
+	LEFT JOIN ORDERS ON
+    		CUSTOMER.C_CUSTKEY = ORDERS.O_CUSTKEY
+
+	GROUP BY C_NAME	)
+as pedidos
+WHERE 
+	pedidos.soma = 0
+;
