8688079bbbd34b3cf0b74432b146fc69e0577774
Vinícius Teixeira Vieira dos Santos <vinisantos185@gmail.com>
2021-06-09 17:09:26 -0300

    Adição de comentários na q8
    Signed-off-by: Vinícius Teixeira Vieira dos Santos <vinisantos185@gmail.com>

--- T1/q8_not_special.sql
+++ T1/q8_not_special.sql
@@ -1,20 +1,25 @@
+-- Explicando a lógica da consulta:
+-- Primeiramente faço uma consulta ao banco buscando os clientes e suas compras.
+-- Caso um cliente tenha uma compra com 'special request', uma "flag" é marcada nele com 1
+-- caso contrário, o cliente é marcado com 0.
+-- Com isso, basta fazer a contagem de clientes sem flag
+
 SELECT
 	COUNT(pedidos.soma)
 FROM (SELECT
-		sum(
-		case 
-			when INSTR(O_COMMENT, 'special request')>0 
-			then 1
-			else 0
-		end)
-		as soma
+	SUM(
+		CASE 
+			WHEN INSTR(O_COMMENT, 'special request')>0 
+			THEN 1
+			ELSE 0
+		END)
+	as soma
 	FROM
 		CUSTOMER
 	LEFT JOIN ORDERS ON
     		CUSTOMER.C_CUSTKEY = ORDERS.O_CUSTKEY
 
-	GROUP BY C_NAME	)
-as pedidos
+	GROUP BY C_NAME
+) as pedidos
 WHERE 
-	pedidos.soma = 0
-;
+	pedidos.soma = 0;
